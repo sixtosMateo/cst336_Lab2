32b5aad777cc0d869a92076d3d1787c8689944e6
rive1941 <marivera@csumb.edu>
2015-02-11 18:59:58 +0000

    added index.php

--- index.php
+++ index.php
@@ -6,3 +6,30 @@
 ?>
 
 
+<DOCTYPE html>
+<html>
+    <head>
+        <title>SilverJack</title>
+        <meta charset="utf-8"/>
+        
+    </head>
+    
+    <body>
+        
+        <?php
+           
+        ?>
+        
+        
+        
+        <footer>
+            &copy; Created by: Maria Rivera
+        </footer>
+        
+    </body>
+    
+</html>
+
+</DOCTYPE>
+
+
