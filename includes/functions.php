86008a7660e166d6df5bd7da7736ce9128c3f523
pesq8691 <dpesqueda@csumb.edu>
2015-02-13 19:15:38 +0000

    Refactored php and divided functionality to make it more readable, also checks for ties now

--- includes/functions.php
+++ includes/functions.php
@@ -55,6 +55,52 @@ function generateGame()
  
 }
 
+function generatePlayField(){
+    $players = array();
+    $images = array("f1","f2","f3", "f4");
+    
+    for($index = 0; $index < count($images); $index++){
+        echo "<img src = 'img/".$images[$index].".png'>";
+        array_push($players,generateGame());
+        echo $players[$index] . "<br>";
+    }
+    
+    return ($players);
+}
+
+function determineWinners($players){
+     $max = max($players);
+     $winner = array();
+
+    for($i = 0; $i < count($players); $i++){
+        if($players[$i] >= $max){
+           array_push($winner,$i);
+        }
+    }
+    
+    if(sizeof($winner) == 1){
+        switch($winner[0]){
+            case 0:
+            echo "Brayanne won!";
+            break;
+            case 1:
+            echo "Maria won!";
+            break;
+            case 2:
+            echo "Mateo won!";
+            break;
+            case 3:
+            echo "Daniel won!";
+            break;
+            default: "Error!";
+            break;
+        }
+    }
+    else{
+        echo "Tie!";
+    }
+}
+
 
 
 ?>
--- index.php
+++ index.php
@@ -10,58 +10,15 @@
         <h1>Let's Play Silver Jack!</h1>
         <?php
            include 'includes/functions.php';
-           $players = array();
-           echo "<img src = 'img/f1.png'>";
-           $player1 = generateGame();
-           echo $player1 . "<br>";
-            
-           echo "<img src = 'img/f2.png'>";
-           $player2 = generateGame();
-           echo $player2 . "<br>";
-
-           echo "<img src = 'img/f3.png'>";
-           $player3 = generateGame();
-           echo $player3 . "<br>";
-
-
-           echo "<img src = 'img/f4.png'>";
-           $player4 = generateGame();
-           echo $player4 . "<br>";
-          
-    
-           array_push($players, $player1, $player2, $player3, $player4);
-           $max = $player1;
-
-
-            for($i = 0; $i < count($players); $i++){
-                if($players[$i] > $max){
-                    $max = $players[$i];
-                    $winner = $i;
-                }
-            }
-
-            
-            switch($winner){
-                case 0:
-                    echo "Brayanne won!";
-                    break;
-                case 1:
-                    echo "Maria won!";
-                    break;
-                case 2:
-                    echo "Mateo won!";
-                    break;
-                case 3:
-                    echo "Daniel won!";
-                    break;
-            }  
+           determineWinners(generatePlayField());
+           
         ?>
         
         <br>
-        <button onClick = "window.open('index.php')" type = "button">Play again!</button>
+        <button onClick = "window.location.reload()" type = "button">Play again!</button>
         
         <footer>
-            &copy; Created by: Maria Rivera
+            &copy; Created by: Maria Rivera, Daniel Pesqueda, Mateo Sixtos, Brayanne Reyes
         </footer>
         
     </body>
