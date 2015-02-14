4d4125cf9455675aa446adc74d5d2cabad2e8d3f
reye6671 <breyesron@csumb.edu>
2015-02-14 04:51:42 +0000

    Styled button and centered everything

--- index.php
+++ index.php
@@ -7,23 +7,27 @@
     </head>
     
     <body>
-        <h1>Let's Play Silver Jack!</h1>
-        <?php
-           include 'includes/functions.php';
-
-
-           $playerScores = generatePlayField();
-           $winnersList = determineWinners($playerScores);
-           printWinMSG($winnersList);
-
-        ?>
         
-        <br>
-        <button onClick = "window.location.reload()" type = "button">Play again!</button>
-        
-        <footer>
-            &copy; Created by: Maria Rivera, Daniel Pesqueda, Mateo Sixtos, Brayanne Reyes
-        </footer>
+        <div>
+            <h1>Let's Play Silver Jack!</h1>
+            <?php
+               include 'includes/functions.php';
+
+
+               $playerScores = generatePlayField();
+               $winnersList = determineWinners($playerScores);
+               printWinMSG($winnersList);
+
+            ?>
+
+            <br>
+            <button onClick = "window.location.reload()" type = "button">Play again!</button>
+
+            <footer>
+                <hr>
+                &copy; Created by: Maria Rivera, Daniel Pesqueda, Mateo Sixtos, Brayanne Reyes
+            </footer>
+        </div>
         
     </body>
     
